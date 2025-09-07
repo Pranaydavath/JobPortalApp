f9b79f3dbf02945658c06c92d34d3966f6453c27
pranay <pranaydavath1416@gmail.com>
2025-09-07 19:51:28 +0530

    Changes

--- src/main/java/com/pranay/JobApp/controller/JobController.java
+++ src/main/java/com/pranay/JobApp/controller/JobController.java
@@ -4,9 +4,12 @@ import com.pranay.JobApp.model.JobPost;
 import com.pranay.JobApp.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class JobController {
@@ -20,16 +23,18 @@ public class JobController {
     public String addJob(){
         return "addjob";
     }
-    @GetMapping("viewalljobs")
-    public String viewJob(){
 
-        return "viewalljobs";
-    }
     @PostMapping("handleForm")
-    public String handleForm(JobPost jobPost) {
-        service.addJob(jobPost);
+    public String handleForm(JobPost jobPosts) {
+        service.addJob(jobPosts);
         return "success";
 
+    }
+    @GetMapping("viewalljobs")
+    public String viewJobs(Model m){
+        List<JobPost> jobs = service.getAllJobs();
+        m.addAttribute("jobPosts",jobs);
+        return "viewalljobs";
     }
 
 }
